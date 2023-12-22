98c19b88b713f92d28e17ee92f84d4af4500554a
munuhee <murichustephen@gmail.com>
2023-12-22 19:48:47 +0300

    add tag model and field

--- movie/models.py
+++ movie/models.py
@@ -14,6 +14,16 @@ from django.db import models
 from django.core.files.base import ContentFile
 from PIL import Image
 
+class Tag(models.Model):
+    """
+    Tag Model represents tags associated with movies.
+    """
+
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
 class Movie(models.Model):
     """
     Movie Model represents information about a movie.
@@ -23,21 +33,29 @@ class Movie(models.Model):
     - genre: The genre of the movie.
     - release_year: The year the movie was released.
     - description: Description or summary of the movie.
+    - directors: The directors of the movie.
+    - country: The country of the movie production.
+    - duration: Duration of the movie in minutes.
     - image: ImageField storing the movie's image.
     - trailer_link: URLField storing the link to the movie's trailer.
     - subscription_required: Indicates if the movie is part of a subscription plan.
-    - created_at: Time the movie was posted
+    - created_at: Time the movie was posted.
+    - tags: Many-to-many relationship with Tag model.
     """
 
     title = models.CharField(max_length=255)
     genre = models.CharField(max_length=100)
     release_year = models.IntegerField()
     description = models.TextField()
+    directors = models.CharField(max_length=255, null=True, blank=True)
+    country = models.CharField(max_length=100, null=True, blank=True)
+    duration = models.IntegerField(null=True, blank=True, verbose_name='Duration in minutes')
     image = models.ImageField(upload_to='movie_images/', blank=True, null=True)
     trailer_link = models.URLField(blank=True, null=True)
     subscription_required = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
-
+    tags = models.ManyToManyField(Tag)
+    
     def __str__(self):
         """
         Returns the title of the movie when it's converted to a string.
