ec19119aa84462fe969caee1d516cdf0ac37e85c
munuhee <murichustephen@gmail.com>
2023-12-22 19:53:35 +0300

    Updated URL paths and configured static settings for enhanced routing and optimized content delivery

--- movie/urls.py
+++ movie/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'movies'
 
 urlpatterns = [
     path('', views.movie_list, name='movie_list'),
+    path('popular/', views.popular_movies, name='popular_movies_list'),
+    path('new-movies/', views.new_movies, name='new_movies_list'),
+    path('tv-series/', views.tv_series, name='tv_series_list'),
     path('create/', views.movie_create, name='movie_create'),
     path('<int:pk>/', views.movie_detail, name='movie_detail'),
     path('<int:pk>/update/', views.movie_update, name='movie_update'),
     path('<int:pk>/delete/', views.movie_delete, name='movie_delete'),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
