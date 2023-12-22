45e601c6594023c2f0fcdb83dc8895cc71ba1f19
munuhee <murichustephen@gmail.com>
2023-12-22 19:51:41 +0300

    Implemented search functionality and tag inclusion for improved user navigation

--- movie/views.py
+++ movie/views.py
@@ -20,14 +20,92 @@ from review.forms import ReviewForm
 from .models import Movie
 from subscription.models import UserSubscription
 from review.models import Review
+from django.db.models import Q
 
 def movie_list(request):
     """Renders a list of movies."""
     movies = Movie.objects.all()
+    popular_movies = Movie.objects.filter(tags__name='most popular')
+    tv_series = Movie.objects.filter(tags__name='tv series')
+    new_movies = Movie.objects.filter(tags__name='new movies')
+    query = request.GET.get('q')
+    
+    if query:
+        movies = movies.filter(
+            Q(title__icontains=query) |
+            Q(genre__icontains=query) |
+            Q(description__icontains=query) |
+            Q(directors__icontains=query) |
+            Q(country__icontains=query)
+        )
+
     context = {
         'movies': movies,
+        'popular_movies':popular_movies,
+        'tv_series':tv_series,
+        'new_movies':new_movies,
+        'query': query,
     }
     return render(request, 'movie/movie_list.html', context)
+
+def popular_movies(request):
+    """Renders a list of popular movies."""
+    popular_movies = Movie.objects.filter(tags__name='most popular')
+    query = request.GET.get('q')
+    
+    if query:
+        popular_movies = popular_movies.filter(
+            Q(title__icontains=query) |
+            Q(genre__icontains=query) |
+            Q(description__icontains=query) |
+            Q(directors__icontains=query) |
+            Q(country__icontains=query)
+        )
+        
+    context = {
+        'popular_movies':popular_movies,
+        'query': query,
+    }
+    return render(request, 'movie/most_popular.html', context)
+
+def tv_series(request):
+    """Renders a list of tv series."""
+    tv_series = Movie.objects.filter(tags__name='tv series')
+    query = request.GET.get('q')
+    
+    if query:
+        tv_series = tv_series.filter(
+            Q(title__icontains=query) |
+            Q(genre__icontains=query) |
+            Q(description__icontains=query) |
+            Q(directors__icontains=query) |
+            Q(country__icontains=query)
+        )
+        
+    context = {
+        'tv_series':tv_series,
+        'query': query,
+    }
+    return render(request, 'movie/tv_series.html', context)
+
+def new_movies(request):
+    """Renders a list of new movies."""
+    new_movies = Movie.objects.filter(tags__name='new movies')
+    query = request.GET.get('q')
+    
+    if query:
+        new_movies = new_movies.filter(
+            Q(title__icontains=query) |
+            Q(genre__icontains=query) |
+            Q(description__icontains=query) |
+            Q(directors__icontains=query) |
+            Q(country__icontains=query)
+        )
+    context = {
+        'new_movies':new_movies,
+        'query': query,
+    }
+    return render(request, 'movie/new_movies.html', context)
 
 def movie_detail(request, pk):
     """Renders details of a specific movie based on its primary key."""
